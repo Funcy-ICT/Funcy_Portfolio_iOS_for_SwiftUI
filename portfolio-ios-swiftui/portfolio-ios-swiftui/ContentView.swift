deba8824350c99b29d814f5f38281d860f87e56d
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2022-05-23 17:18:00 +0900

    first commit

---
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  portfolio-ios-swiftui
+//
+//  Created by 鳥山英峻 on 2022/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/portfolio_ios_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolio_ios_swiftuiApp.swift
+//  portfolio-ios-swiftui
+//
+//  Created by 鳥山英峻 on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct portfolio_ios_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
