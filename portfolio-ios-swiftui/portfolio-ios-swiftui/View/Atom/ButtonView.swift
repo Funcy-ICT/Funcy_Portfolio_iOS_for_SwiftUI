6009faa079671c78fd7ca23b055e1b44e3e19f85
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-07-05 01:09:37 +0900

    [fix]共通化したButtonに置き換え

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/ButtonView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/ButtonView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 struct ButtonView: View {
     @State var label: String
 
-    enum ColorPattern: Int {
-        case normal = 0
-        case invite = 1
-        case new = 2
-        case wip = 3
+    enum ColorPattern {
+        case normal
+        case invite
+        case new
+        case wip
+        
     }
 
-    let colorPattern: ColorPattern.RawValue
+    let colorPattern: ColorPattern
 
     let gradientView = LinearGradient(
         gradient: Gradient(colors: [Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255),
@@ -30,85 +31,75 @@ struct ButtonView: View {
         let widthSize = getWidthSize(text: words)
 
         switch colorPattern {
-        case ColorPattern.normal.rawValue:
-            Button(action: {
-                print("押した")
-            }) {
-                Text(words)
-                    .font(.system(size: 20))
-                    .foregroundColor(Color.white)
-                    .lineLimit(1)
-            }
-            .frame(width: widthSize, height: 50)
+        case ColorPattern.normal:
+            CommonButtonView(action: { print("保存")},
+                             labelText: words,
+                             width: widthSize,
+                             foregroundColor: Color.white, overlay: EmptyView()
+            )
             .background(gradientView)
             .cornerRadius(25)
             .padding(.horizontal, 30)
 
-        case ColorPattern.invite.rawValue:
-            Button(action: {
-                print("押した")
-            }) {
-                Text(words)
-                    .font(.system(size: 20))
-                    .frame(width: widthSize, height: 50)
-                    .foregroundColor(Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255), lineWidth: 2)
-                    )
-                    .lineLimit(1)
-            }
-        case ColorPattern.new.rawValue:
-            Button(action: {
-                print("押した")
-            }) {
-                Text(words)
-                    .font(.system(size: 20))
-                    .frame(width: widthSize, height: 50)
-                    .foregroundColor(Color(red: 112 / 255, green: 127 / 255, blue: 137 / 255))
-                    .background(Color(red: 229 / 255, green: 229 / 255, blue: 229 / 255))
-                    .cornerRadius(25)
-                    .lineLimit(1)
-            }
+        case ColorPattern.invite:
+            CommonButtonView(
+                action: { print("招待")},
+                labelText: words,
+                width: widthSize,
+                foregroundColor: Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255),
+                overlay:
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255), lineWidth: 2)
+            )
+            
+        case ColorPattern.new:
+            CommonButtonView(
+                action: { print("下書き")},
+                labelText: words,
+                width: widthSize,
+                foregroundColor: Color(red: 112 / 255, green: 127 / 255, blue: 137 / 255),
+                backgroundColor: Color(red: 229 / 255, green: 229 / 255, blue: 229 / 255),
+                overlay: EmptyView(),
+                radius: 20.0
+            )
+            
         default:
-            Button(action: {
-                print("押した")
-            }) {
-                Text(words)
-                    .font(.system(size: 20))
-                    .foregroundColor(Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255))
-                    .lineLimit(1)
-            }
+            CommonButtonView(
+                action: { print("新規会員登録")},
+                labelText: words,
+                width: widthSize,
+                foregroundColor: Color(red: 255 / 255, green: 144 / 255, blue: 179 / 255),
+                overlay: EmptyView()
+            )
         }
     }
-}
-
-func getWidthSize(text: String) -> CGFloat {
-
-    let textCount = text.count
-    var widthSize: CGFloat
-    if textCount > 5 {
-        widthSize = 190
-    } else {
-        widthSize = 100
+    
+    private func getWidthSize(text: String) -> CGFloat {
+        
+        var widthSize: CGFloat
+        if text.count > 5 {
+            widthSize = 190
+        } else {
+            widthSize = 100
+        }
+        // 日本語7文字以上は改行になる, エラーハンドリング
+        return widthSize
     }
-    // 日本語7文字以上は改行になる, エラーハンドリング
-    return widthSize
-}
 
-func limit(text: String) -> String {
-    var word = text
-    word = String(word.prefix(7))
-    return word
+    private func limit(text: String) -> String {
+        var word = text
+        word = String(word.prefix(7))
+        return word
+    }
 }
 
 struct ButtonView_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 30) {
-            ButtonView(label: "保存保存保存保存保", colorPattern: 0)
-            ButtonView(label: "招待", colorPattern: 1)
-            ButtonView(label: "下書き", colorPattern: 3)
-            ButtonView(label: "新規会員登録", colorPattern: 2)
+            ButtonView(label: "保存保存保存保存保", colorPattern: .normal)
+            ButtonView(label: "招待", colorPattern: .invite)
+            ButtonView(label: "下書き", colorPattern: .wip)
+            ButtonView(label: "新規会員登録", colorPattern: .new)
         }
     }
 }
