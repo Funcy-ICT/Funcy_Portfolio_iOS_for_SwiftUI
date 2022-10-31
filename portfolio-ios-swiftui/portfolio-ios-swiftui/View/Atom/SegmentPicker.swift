8ebab5aeeaed8a44766fc1e34781d81b61950120
massu116 <kakeru1999116@yahoo.co.jp>
2022-10-31 17:10:54 +0900

    [fix] マイページ・グループページ等3パターン追加

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/SegmentPicker.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/SegmentPicker.swift
@@ -9,24 +9,71 @@ import SwiftUI
 
 struct SegmentPicker: View {
     @State private var selectedIndex = 0
+        //var labelName: [String] = []
+    
+    enum StatusPattern{
+        case privatePage
+        case groupPage
+        case groupMyPage
+    }
+    let statusPattern: StatusPattern
 
     var body: some View {
+               
         VStack {
-            Picker("", selection: self.$selectedIndex) {
-                Text("作品一覧").tag(0)
-                Text("プロフィール").tag(1)
-                Text("スキル").tag(2)
+            switch statusPattern{
+            case .privatePage:
+                Picker("", selection: self.$selectedIndex) {
+                    Text("作品一覧").tag(0)
+                    Text("グループ紹介").tag(1)
+                    Text("メンバー 一覧").tag(2)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(10)
+
+                switch selectedIndex {
+                case 0:
+                    Text("hoge")    //後ほどアクションを定義
+                case 1:
+                    Text("fuga")
+                default:
+                    Text("piyo")
+                }
+            case .groupPage:
+                Picker("", selection: self.$selectedIndex) {
+                    Text("作品一覧").tag(0)
+                    Text("プロフィール").tag(1)
+                    Text("スキル").tag(2)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(10)
+
+                switch selectedIndex {
+                case 0:
+                    Text("hogehoge")
+                case 1:
+                    Text("fugafuga")
+                default:
+                    Text("piyopiyo")
+                }
+            case .groupMyPage:
+                Picker("", selection: self.$selectedIndex) {
+                    Text("作品一覧").tag(0)
+                    Text("グループ紹介").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(10)
+
+                switch selectedIndex {
+                case 0:
+                    Text("hoge")
+                case 1:
+                    Text("fuga")
+                default:
+                    Text("piyo")
+                }
             }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding(10)
-            switch selectedIndex {
-            case 0:
-                Text("hoge")
-            case 1:
-                Text("fuga")
-            default:
-                Image("momogamisama")
-            }
+            
         }
 
     }
@@ -34,6 +81,12 @@ struct SegmentPicker: View {
 
 struct SegmentPicker_Previews: PreviewProvider {
     static var previews: some View {
-        SegmentPicker()
+        //let label = ["作品一覧", "プロフィール", "スキル"]
+        VStack(spacing: 30){
+            SegmentPicker(statusPattern: .privatePage)
+            SegmentPicker(statusPattern: .groupPage)
+            SegmentPicker(statusPattern: .groupMyPage)
+        }
+        
     }
 }
