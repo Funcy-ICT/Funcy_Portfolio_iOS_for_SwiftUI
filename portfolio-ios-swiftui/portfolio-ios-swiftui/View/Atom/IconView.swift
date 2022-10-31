1ef1a41cb428425d86d5a09d7cc74e3d950fdaf0
Mori Haruna <miraidaigakukoudo@codepro2019MBA02.local>
2022-10-31 17:33:50 +0900

    [fix]caseなどの修正

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/IconView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/IconView.swift
@@ -7,52 +7,44 @@
 
 import SwiftUI
 
-
 enum IconPattern {
     case large
     case small
 }
 
 struct IconView: View {
-    
-    let imageName: String // TODO: 将来的にはURLに置き換える
+
+    // Todo: 将来的にURLに置き換える
+    let imageName: String
     let iconPattern: IconPattern
-    
+
     var body: some View {
-        
+
         switch iconPattern {
-            
+
         case .large:
             Image(imageName)
                 .resizable()
                 .clipShape(Circle())
                 .scaledToFit()
                 .frame(width: 148.0, height: 148.0)
-                // 74 × 2 = 148.0
-            
-        case .small:
-            Image(imageName)
-                .resizable()
-                .clipShape(Circle())
-                .scaledToFit()
-                .frame(width: 108.0, height: 108.0)
-                // 54 × 2 = 108.0
-            
+        // 74 × 2 = 148.0
+
         default:
             Image(imageName)
                 .resizable()
                 .clipShape(Circle())
                 .scaledToFit()
-                .frame(width: 148.0, height: 148.0)
-            
+                .frame(width: 108.0, height: 108.0)
+        // 54 × 2 = 108.0
         }
     }
 }
 
-#if DEBUG
-struct IconView_Previews: PreviewProvider {
-    static var previews: some View {
-        IconView(imageName: "IMG_6788", iconPattern: .small)
-    }
-}
-#endif
+// #if DEBUG
+// struct IconView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        IconView(imageName: "IMG_6788", iconPattern: .large)
+//    }
+// }
+// #endif
