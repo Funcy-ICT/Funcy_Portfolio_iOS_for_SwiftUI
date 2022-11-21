155b2c858c38d6215f8f3166ac5a8f774cb4b144
mersakakey <75288670+mersakakey@users.noreply.github.com>
2022-11-21 17:19:29 +0900

    [fix] インデントを修正

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Organism/TabBarView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Organism/TabBarView.swift
@@ -12,34 +12,34 @@ struct TabBarView: View {
     init() {
         // TabViewの背景色の設定
         UITabBar.appearance().backgroundColor = UIColor(Color.tabbarColor)
-        
     }
     
     var body: some View {
+        
         TabView {
+            
             TestView1() // メイン画面のView
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                     Text("検索")
-                    
                 }
+            
             TestView2() // マイページ（個人）のView
                 .tabItem {
                     Image(systemName: "person")
                     Text("ユーザー")
-                    
                 }
+            
             TestView1() // マイページ（グループ）のView
                 .tabItem {
                     Image(systemName: "person.3")
                     Text("グループ")
-                    
                 }
+            
             TestView2() // 設定画面のView
                 .tabItem {
                     Image(systemName: "gearshape")
                     Text("設定")
-                    
                 }
             
         }.accentColor(.textPinkColor)
@@ -49,9 +49,11 @@ struct TabBarView: View {
 }
 
 struct TabBarView_Previews: PreviewProvider {
+    
     static var previews: some View {
         TabBarView()
             .previewInterfaceOrientation(.portrait)
         
     }
+    
 }
