ae8ac9da6e1f3b11b95870416fe09d0a9562f00c
massu116 <kakeru1999116@yahoo.co.jp>
2022-11-21 15:35:10 +0900

    [add] コンフリクト解消のために新たにsegmentPickerを追加

---
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/SegmentPicker.swift
@@ -0,0 +1,69 @@
+//
+//  SegmentPicker.swift
+//  portfolio-ios-swiftui
+//
+//  Created by fun_massu on 2022/11/21.
+//
+
+import SwiftUI
+
+struct SegmentPicker: View {
+    @State private var selectedIndex = 0
+
+    enum StatusPattern {
+        case myPage
+        case groupPage
+    }
+
+    let statusPattern: StatusPattern
+
+    var body: some View {
+        VStack {
+            switch statusPattern {
+            case .myPage:
+                Picker("", selection: self.$selectedIndex) {
+                    Text("作品一覧").tag(0)
+                    Text("プロフィール").tag(1)
+                    Text("スキル").tag(2)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(10)
+
+                switch selectedIndex {
+                case 0:
+                    Text("hoge")    // 後ほどアクションを定義
+                case 1:
+                    Text("fuga")
+                default:
+                    Text("piyo")
+                }
+
+            case .groupPage:
+                Picker("", selection: self.$selectedIndex) {
+                    Text("作品一覧").tag(0)
+                    Text("グループ紹介").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(10)
+
+                switch selectedIndex {
+                case 0:
+                    Text("hoge")
+                case 1:
+                    Text("fuga")
+                default:
+                    Text("piyo")
+                }
+            }
+        }
+    }
+}
+
+struct SegmentPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 30) {
+            SegmentPicker(statusPattern: .myPage)
+            SegmentPicker(statusPattern: .groupPage)
+        }
+    }
+}
