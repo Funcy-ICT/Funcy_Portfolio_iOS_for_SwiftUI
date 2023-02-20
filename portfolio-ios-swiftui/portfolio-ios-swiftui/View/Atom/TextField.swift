762d6e96113fb271833de89c65ca3f69c44347d0
zekuta-x <rem_lim11@icloud.com>
2023-02-20 13:22:16 +0900

    [fix] atom/textfieldの修正

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
@@ -4,28 +4,46 @@
 //
 // Created by fun_massu on 2022/09/26.
 //
-import SwiftUI
-struct TextBox: View {
-    @State private var inputText = ""
-    let text: String
-    var body: some View {
 
+import SwiftUI
+
+struct TextBox: View {
+    
+    let fieldHide: Bool
+    let titleText: String
+    let descriptionText: String
+    
+    @Binding var inputText: String
+    
+    var body: some View {
         VStack(alignment: .leading) {
-            Text(text)
-                .foregroundColor(Color.text)
-            TextField("入力してください", text: $inputText)
-                .frame(width: 240, height: 40)
-                .padding(.leading, 15)
-                .overlay(RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.grayBottonColor, lineWidth: 2)
-                )
-                .padding(.leading, 25)
+            TextView(text: titleText, textPattern: 0)
+            
+            if fieldHide {
+                SecureField(descriptionText, text: $inputText)
+                    .frame(width: 294, height: 48)
+                    .padding(.leading, 15)
+                    .overlay(RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.grayBottonColor, lineWidth: 2)
+                    )
+                    .padding(.leading, 25)
+            } else {
+                TextField(descriptionText, text: $inputText)
+                    .frame(width: 294, height: 48)
+                    .padding(.leading, 15)
+                    .overlay(RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.grayBottonColor, lineWidth: 2)
+                    )
+                    .padding(.leading, 25)
+            }
         }
     }
 }
 
-struct TextBox_Previews: PreviewProvider {
-    static var previews: some View {
-        TextBox(text: "メールアドレス")
-    }
-}
+//struct TextBox_Previews: PreviewProvider {
+//    @State private var test = ""
+//
+//    static var previews: some View {
+//        TextBox(titleText: "メールアドレス", inputText: $test)
+//    }
+//}
