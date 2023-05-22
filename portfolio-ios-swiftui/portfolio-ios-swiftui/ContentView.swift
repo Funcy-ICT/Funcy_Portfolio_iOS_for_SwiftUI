e389c320f46f86d911704aa9c7dccdd232c74f5a
massu116 <kakeru1999116@yahoo.co.jp>
2023-05-22 16:42:04 +0900

    [remove]存在していない写真を削除, ContentViewから使用されていないImage関数を削除

--- portfolio-ios-swiftui/portfolio-ios-swiftui/ContentView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/ContentView.swift
@@ -9,12 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("IMG_6788")
-            .resizable()
-            .clipShape(Circle())
-            .scaledToFit()
-            .frame(width: 120.0, height: 100.0)
-            .position(x: 70, y: 70)
+        Text("Hello, world!")
     }
 }
 
