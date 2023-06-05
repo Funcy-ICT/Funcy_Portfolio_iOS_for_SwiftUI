7d2e9c9cc3ab2229eac7e00302a86b7ff9689743
Kakeru Masuda <kakeru1999116@yahoo.co.jp>
2023-06-06 04:03:17 +0900

    [add] パスワード入力用のtextFieldを追加

--- portfolio-ios-swiftui/portfolio-ios-swiftui/ContentView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/ContentView.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("IMG_6788")
-            .resizable()
-            .clipShape(Circle())
-            .scaledToFit()
-            .frame(width: 120.0, height: 100.0)
-            .position(x: 70, y: 70)
+        VStack(spacing: 20) {
+            TextBox(text: "メールアドレス", textFieldPattern: 0)
+            TextBox(text: "パスワード", textFieldPattern: 1)
+        }
     }
 }
 
--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
@@ -7,25 +7,90 @@
 import SwiftUI
 struct TextBox: View {
     @State private var inputText = ""
+    
+    enum TextFieldPattern: Int {
+        case normal = 0
+        case hide = 1
+    }
+
     let text: String
+    let textFieldPattern: TextFieldPattern.RawValue
+    @State  var show: Bool = false
+    @FocusState  var focusText: Bool
+    @FocusState  var focusSecure: Bool
+    
     var body: some View {
 
-        VStack(alignment: .leading) {
-            Text(text)
-                .foregroundColor(Color.text)
-            TextField("入力してください", text: $inputText)
-                .frame(width: 240, height: 40)
-                .padding(.leading, 15)
-                .overlay(RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.grayBottonColor, lineWidth: 2)
-                )
-                .padding(.leading, 25)
+        switch textFieldPattern{
+        case TextFieldPattern.hide.rawValue:
+            
+            VStack(alignment: .leading) {
+                Text(text)
+                    .foregroundColor(Color.text)
+                
+                HStack{
+                    ZStack(alignment: .trailing) {
+                        TextField("入力してください", text: $inputText)
+                            .autocapitalization(.none)
+                            .focused($focusText)
+                            .opacity(show ? 1 : 0)
+                            .frame(width: 240, height: 40)
+                            .padding(.leading, 15)
+                            .overlay(RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.grayBottonColor, lineWidth: 2)
+                            )
+                            .padding(.leading, 25)
+                        
+                        SecureField("入力してください", text: $inputText)
+                            .autocapitalization(.none)
+                            .focused($focusText)
+                            .opacity(show ? 0 : 1)
+                            .frame(width: 240, height: 40)
+                            .padding(.leading, 15)
+                            .overlay(RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.grayBottonColor, lineWidth: 2)
+                            ).padding(.leading, 25)
+                         
+                        Button(action: {
+                            show.toggle()
+                            if show {
+                                focusText = true
+                            } else {
+                                focusSecure = true
+                            }
+                        }, label: {
+                            Image(systemName: self.show ? "eye.slash.fill" : "eye.fill")
+                            .padding()
+                            .font(.system(size: 15))
+                            
+                        })
+                        }
+                    }
+                }
+        
+        default:
+            VStack(alignment: .leading) {
+                Text(text)
+                    .foregroundColor(Color.text)
+                TextField("入力してください", text: $inputText)
+                    .autocapitalization(.none)
+                    .focused($focusText)
+                    .frame(width: 240, height: 40)
+                    .padding(.leading, 15)
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.grayBottonColor, lineWidth: 2)
+                    )
+                    .padding(.leading, 25)
+            }
         }
     }
 }
 
 struct TextBox_Previews: PreviewProvider {
     static var previews: some View {
-        TextBox(text: "メールアドレス")
+        VStack(spacing: 20) {
+            TextBox(text: "メールアドレス", textFieldPattern: 0)
+            TextBox(text: "パスワード", textFieldPattern: 1)
+        }
     }
 }
