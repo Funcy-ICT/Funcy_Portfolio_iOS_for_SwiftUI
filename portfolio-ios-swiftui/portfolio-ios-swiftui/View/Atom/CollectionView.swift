93f788e2d26c77133faf20e7c3e4c46404e4314c
Kakeru Masuda <kakeru1999116@yahoo.co.jp>
2023-06-19 16:18:55 +0900

    [fix] 画像の読み込みをAsyncImageに変更

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/CollectionView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/CollectionView.swift
@@ -11,17 +11,24 @@ struct CustomCell: View, Identifiable {
     var id = UUID()
     var title: String
     var content: String
-    var imgPath: String
+    let urlString: String
     
     var body: some View {
         ZStack(alignment: .bottomLeading) {
-            Image(imgPath)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 150, height: 150, alignment: .top)
-                .clipped()
-                .cornerRadius(20)
-
+            let imgPath = URL(string: urlString)
+            AsyncImage(url: imgPath) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 100, alignment: .top)
+                        .clipped()
+                        .cornerRadius(20)
+                } else if let error = phase.error {
+                    let _ = print(error.localizedDescription)
+                }
+            }
+            .padding(.bottom, 50)
             VStack(alignment: .leading, spacing: 4) {
                 TextView(text: title, textPattern: 3).foregroundColor(.text).padding(.leading, 10)
                 TextView(text: content, textPattern: 0).foregroundColor(.text).padding(.leading, 10)
@@ -76,25 +83,33 @@ struct CollectionView: View {
     }
 }
 
-/*
 #if DEBUG
 struct CollectionView_Previews: PreviewProvider {
     static var previews: some View {
         let data: [CustomCell] = [
-            CustomCell(title: "garden_strand", content: "ネックレス", imgPath: "garden_strand"),
-            CustomCell(title: "gatsby_hat", content: "ハット", imgPath: "gatsby_hat"),
-            CustomCell(title: "stella_sunglasses", content: "グラス", imgPath: "stella_sunglasses"),
-            CustomCell(title: "strut_earrings", content: "イヤリング", imgPath: "strut_earrings"),
-            CustomCell(title: "vagabond_sack", content: "リュックサック", imgPath: "vagabond_sack"),
-            CustomCell(title: "varsity_socks", content: "ソックス", imgPath: "varsity_socks"),
-            CustomCell(title: "whitey_belt", content: "ベルト", imgPath: "whitey_belt"),
-            CustomCell(title: "copper_wire_rack", content: "ラック", imgPath: "copper_wire_rack"),
-            CustomCell(title: "gilt_desk_trio", content: "小物入れ", imgPath: "gilt_desk_trio"),
-            CustomCell(title: "shrug_bag", content: "バッグ", imgPath: "shrug_bag")
+            CustomCell(
+                title: "bag",
+                content: "バッグ",
+                urlString: "https://www.totebag.jp/img/01product/01ready-made/H50110.jpg"
+            ),
+            CustomCell(
+                title: "bag",
+                content: "バッグ",
+                urlString: "https://www.totebag.jp/img/01product/01ready-made/H50110.jpg"
+            ),
+            CustomCell(
+                title: "bag",
+                content: "バッグ",
+                urlString: "https://www.totebag.jp/img/01product/01ready-made/H50110.jpg"
+            ),
+            CustomCell(
+                title: "bag",
+                content: "バッグ",
+                urlString: "https://www.totebag.jp/img/01product/01ready-made/H50110.jpg"
+            )
         ]
         
         CollectionView(item: data)
     }
 }
 #endif
-*/
