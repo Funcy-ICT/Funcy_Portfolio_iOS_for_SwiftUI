ac3e350f7f7199cf65f523623343d4506d23941d
mersakakey <75288670+mersakakey@users.noreply.github.com>
2023-06-26 17:58:20 +0900

    [fix]とりあえずatomを配置

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Molecules/PostView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Molecules/PostView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct PostView: View {
+    @State private var publishing = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Button(action: {
+                    // ここにアクションを追加する
+                }) {
+                    Image(systemName: "lessthan")
+                        .resizable()
+                        .frame(width: 25, height: 40)
+                        .foregroundColor(.gray)
+                }
+                IconView(imageName: "testpic", iconPattern: .small)
+                BaseButtonView(
+                    action: { print("何か処理をするよー") },
+                    labelText: "下書き",
+                    foregroundColor: Color.textPinkColor,
+                    backgroundColor: Color.clear, radius: 25, lineColor: Color.textPinkColor
+                )
+                BaseButtonView(
+                    action: { print("何か処理をするよー") },
+                    labelText: "投稿",
+                    foregroundColor: Color.white,
+                    backgroundColor: Color.subPink, radius: 25
+                )
+            }
+            Divider()
+                .frame(width: 350, height: 1)
+                .background(Color.black)
+            Text("公開設定")
+                .padding(.leading, 22)
+                .foregroundColor(Color.text)
+            Toggle("公開", isOn: $publishing)
+                .toggleStyle(SwitchToggleStyle(tint: .textPinkColor))
+                .frame(width: 100)
+                .padding(.leading, 30)
+            TextBox(text: "タイトル")
+                .padding(20)
+            TextBox(text: "説明文")
+                .padding(20)
+            TextBox(text: "Githubリンク")
+                .padding(20)
+            TextBox(text: "Youtubeリンク")
+                .padding(20)
+            // tagのviewはここに
+            
+            // 写真追加も
+            
+        }
     }
 }
 
