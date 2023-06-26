961b0c462b12c8874290b1be0545649caca67d03
mersakakey <75288670+mersakakey@users.noreply.github.com>
2023-06-26 17:00:36 +0900

    PostView.swiftを追加

---
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Molecules/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  portfolio-ios-swiftui
+//
+//  Created by Takeru Sakakibara on 2023/06/26.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
