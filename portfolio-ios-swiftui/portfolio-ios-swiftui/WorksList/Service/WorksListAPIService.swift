ae604d09dc5af169c4303f2c253a05e6c64273bf
Kakeru Masuda <kakeru1999116@yahoo.co.jp>
2023-08-03 14:41:52 +0900

    [add] ViewModelとServiceのファイルを追加

---
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/WorksList/Service/WorksListAPIService.swift
@@ -0,0 +1,8 @@
+//
+//  WorksListAPIService.swift
+//  portfolio-ios-swiftui
+//
+//  Created by massu on 2023/08/03.
+//
+
+import Foundation
---
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/WorksList/ViewModel/WorksListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WorksListViewModel.swift
+//  portfolio-ios-swiftui
+//
+//  Created by massu on 2023/08/03.
+//
+
+import Foundation
