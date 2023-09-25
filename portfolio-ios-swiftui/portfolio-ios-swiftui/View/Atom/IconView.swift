2e57ea324d5957f7d4e51f20ad18d4f59a91bee7
Hidetaka Toriyama <56245434+zekuta-x@users.noreply.github.com>
2023-09-25 22:27:18 +0900

    Merge pull request #63 from Funcy-ICT/Atom/fix/AsyncIconView
    [fix] asyncImageに変更

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/IconView.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/IconView.swift
@@ -15,7 +15,8 @@ enum IconPattern {
 struct IconView: View {
     
     // Todo: 将来的にURLに置き換える
-    let imageName: URL
+    let imageName: URL?
+    
     let iconPattern: IconPattern
     
     var body: some View {
@@ -23,30 +24,34 @@ struct IconView: View {
         switch iconPattern {
             
         case .large:
-            AsyncImage(url: imageName) { image in
-                image
-                    .resizable()
-                    .clipShape(Circle())
-                    .frame(width: 148.0, height: 148.0)
-                    .scaledToFit()
-                    
-            } placeholder: {
-                ProgressView()
+            AsyncImage(url: imageName) { item in
+                if let image = item.image {
+                    image.resizable()
+                        .clipShape(Circle())
+                        .scaledToFit()
+                        .frame(width: 74.0, height: 74.0)
+                }
             }
-            // 74 × 2 = 148.0
             
         case .small:
-            AsyncImage(url: imageName) { image in
-                image
-                    .resizable()
-                    .clipShape(Circle())
-                    .frame(width: 108.0, height: 108.0)
-                    .scaledToFit()
-                    
-            } placeholder: {
-                ProgressView()
+            AsyncImage(url: imageName) { item in
+                if let image = item.image {
+                    image.resizable()
+                        .clipShape(Circle())
+                        .scaledToFit()
+                        .frame(width: 54.0, height: 54.0)
+                }
             }
-            // 54 × 2 = 108.0
+            
+        case .tiny:
+            AsyncImage(url: imageName) { item in
+                if let image = item.image {
+                    image.resizable()
+                        .clipShape(Circle())
+                        .scaledToFit()
+                        .frame(width: 44.0, height: 44.0)
+                }
+            }
         }
     }
 }
@@ -54,7 +59,10 @@ struct IconView: View {
 // #if DEBUG
 // struct IconView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        IconView(imageName: "IMG_6788", iconPattern: .small)
+//        VStack {
+//            IconView(imageName: URL(string: "http://localhost:3004/9efbc5ad-23a6-4f7b-8133-93a8ed128f2cmasuda.jpeg"), iconPattern: .large)
+//
+//        }
 //    }
 // }
 // #endif
