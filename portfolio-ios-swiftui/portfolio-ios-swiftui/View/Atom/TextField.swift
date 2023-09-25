be56d516877fcc9227b36bc6a2a6f7f65f2342e1
mersakakey <75288670+mersakakey@users.noreply.github.com>
2023-09-25 18:03:06 +0900

    [change] いい感じに改行できるように

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
@@ -8,35 +8,53 @@ import SwiftUI
 struct TextBox: View {
     @State private var inputText = ""
     let text: String
-    var width: CGFloat = 240
+    var width: CGFloat = 250
     var height: CGFloat = 40
+    var lines: ClosedRange = 1...1
+    @FocusState  var isActive: Bool
     
     var body: some View {
         
-        VStack(alignment: .leading) {
-            Text(text)
-                .foregroundColor(Color.text)
-            TextField("入力してください", text: $inputText)
-                .autocapitalization(.none)
-                .frame(width: width, height: height)
-                .padding(.leading, 15)
-                .overlay(RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.grayBottonColor, lineWidth: 2)
-                )
-                .padding(.leading, 25)
+        if lines == 1...1 {
+            VStack(alignment: .leading) {
+                Text(text)
+                    .foregroundColor(Color.text)
+                TextField("入力してください", text: $inputText)
+                    .autocapitalization(.none)
+                    .frame(width: width, height: height)
+                    .lineLimit(lines)
+                    .padding(.leading, 10)
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.grayBottonColor, lineWidth: 2))
+                    .padding(.leading, 25)
+            }
+        } else {
+            VStack(alignment: .leading) {
+                Text(text)
+                    .foregroundColor(Color.text)
+                TextField("入力してください", text: $inputText, axis: .vertical)
+                    .autocapitalization(.none)
+                    .frame(width: width)
+                    .padding(.top, 10)
+                    .lineLimit(lines)
+                    .padding(.leading, 10)
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.grayBottonColor, lineWidth: 2))
+                    .padding(.leading, 25)
+            }
         }
     }
 }
 
-//#if DEBUG
-//struct TextBox_Previews: PreviewProvider {
-//    static var previews: some View {
-//        VStack {
-//            TextBox(text: "メールアドレス")
-//            TextBox(text: "メールアドレス", width: 300)
-//            TextBox(text: "メールアドレス", height: 80)
-//            TextBox(text: "メールアドレス", width: 200, height: 200)
-//        }
-//    }
-//}
-//#endif
+#if DEBUG
+struct TextBox_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(alignment: .leading) {
+            TextBox(text: "メールアドレス")
+            TextBox(text: "メールアドレス", width: 300)
+            TextBox(text: "メールアドレス", lines: 6...6)
+            TextBox(text: "メールアドレス", width: 200, lines: 5...5)
+        }
+    }
+}
+#endif
