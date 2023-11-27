d971e431449284eb1cf102ca531f82271624e67f
mersakakey <75288670+mersakakey@users.noreply.github.com>
2023-11-27 16:19:12 +0900

    [add]セキュアなtextfieldのオプションを追加 & 見た目の修正

--- portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
+++ portfolio-ios-swiftui/portfolio-ios-swiftui/View/Atom/TextField.swift
@@ -6,39 +6,50 @@
 //
 import SwiftUI
 struct TextBox: View {
-    @State private var inputText = ""
-    let text: String
-    var width: CGFloat = 250
-    var height: CGFloat = 40 // TextFieldの時にのみ使用可能
+    @Binding var inputText: String
+    var titleText: String
+    var descriptionText: String = "入力してください"
+    var fieldHide: Bool = false
+    var width: CGFloat = 294
+    var height: CGFloat = 48
     var lines: ClosedRange = 1...1
     @FocusState var focus: Bool
     
     var body: some View {
         
-        if lines == 1...1 {
+        if lines == 1...1 { // 単一行のTextField
             VStack(alignment: .leading) {
-                Text(text)
-                    .foregroundColor(Color.text)
-                TextField("入力してください", text: $inputText)
-                    .autocapitalization(.none)
-                    .frame(width: width, height: height)
-                    .lineLimit(lines)
-                    .padding(.leading, 10)
-                    .overlay(RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.grayBottonColor, lineWidth: 2))
-                    .padding(.leading, 25)
+                TextView(text: titleText, textPattern: 0)
+
+                if fieldHide {
+                    SecureField(descriptionText, text: $inputText)
+                        .autocapitalization(.none)
+                        .frame(width: width, height: height)
+                        .padding(.leading, 15)
+                        .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.grayBottonColor, lineWidth: 2))
+                        .padding(.leading, 25)
+                } else {
+                    TextField(descriptionText, text: $inputText)
+                        .autocapitalization(.none)
+                        .frame(width: width, height: height)
+                        .padding(.leading, 15)
+                        .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.grayBottonColor, lineWidth: 2))
+                        .padding(.leading, 25)
+                }
             }
-        } else {
+        } else { // 複数行のTextField
             VStack(alignment: .leading) {
-                Text(text)
+                Text(titleText)
                     .foregroundColor(Color.text)
-                TextField("入力してください", text: $inputText, axis: .vertical)
+                TextField(descriptionText, text: $inputText, axis: .vertical)
                     .autocapitalization(.none)
                     .frame(width: width)
                     .padding(.top, 10)
                     .lineLimit(lines)
-                    .padding(.leading, 10)
-                    .overlay(RoundedRectangle(cornerRadius: 20)
+                    .padding(.leading, 15)
+                    .overlay(RoundedRectangle(cornerRadius: 15)
                         .stroke(Color.grayBottonColor, lineWidth: 2))
                     .padding(.leading, 25)
             }
@@ -46,15 +57,16 @@ struct TextBox: View {
     }
 }
 
-// #if DEBUG
-// struct TextBox_Previews: PreviewProvider {
-//     static var previews: some View {
-//         VStack(alignment: .leading) {
-//             TextBox(text: "メールアドレス")
-//             TextBox(text: "メールアドレス", width: 300)
-//             TextBox(text: "メールアドレス", lines: 6...6)
-//             TextBox(text: "メールアドレス", width: 200, lines: 5...5)
-//         }
-//     }
-// }
-// #endif
+//#if DEBUG
+//struct TextBox_Previews: PreviewProvider {
+//    static var previews: some View {
+//        VStack(alignment: .leading) {
+//            TextBox(inputText: .constant(""), titleText: "メールアドレス")
+//            TextBox(inputText: .constant(""), titleText: "パスワード", descriptionText: "8文字以上記号必須", fieldHide: true)
+//            TextBox(inputText: .constant(""), titleText: "メールアドレス", width: 300)
+//            TextBox(inputText: .constant(""), titleText: "メールアドレス", lines: 6...6)
+//            TextBox(inputText: .constant(""), titleText: "メールアドレス", width: 200, lines: 5...5)
+//        }
+//    }
+//}
+//#endif
